23a0c57d46bf7ba24c5998aaaeaa8e0245b9d085
Dongsoo Lee <dongsoolee2@gmail.com>
2020-01-25 23:18:56 -0800

    mat2movie.m change for color channel

--- d213/utils/mat2movie.m
+++ d213/utils/mat2movie.m
@@ -1,12 +1,28 @@
 function mat2movie(mat)
-% time, x, y
-[T, x, y] = size(mat);
-% generate video file
-v = VideoWriter('sample.mp4', 'MPEG-4');
-open(v)
-for t = 1:T
-    fr = squeeze(mat(t, :, :));
-    writeVideo(v, fr);
+if ndims(mat) ==  4
+    % time, color, x, y
+    [T, c, x, y] = size(mat);
+    % generate video file
+    v = VideoWriter('sample.mp4', 'MPEG-4');
+    v.Quality = 100;
+    open(v)
+    for t = 1:T
+        fr = squeeze(mat(t, :, :, :));
+        fr = permute(fr, [2, 3, 1]);
+        writeVideo(v, fr);
+    end
+    close(v)
+else
+    % time, x, y
+    [T, x, y] = size(mat);
+    % generate video file
+    v = VideoWriter('sample.mp4', 'MPEG-4');
+    v.Quality = 100;
+    open(v)
+    for t = 1:T
+        fr = squeeze(mat(t, :, :));
+        writeVideo(v, fr);
+    end
+    close(v)
 end
-close(v)
 end
