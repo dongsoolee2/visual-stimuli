81012f07b40df78819954359a562f40bf96502bb
Dongsoo Lee <dongsoolee2@gmail.com>
2020-07-20 01:21:36 -0700

    minor edit

--- d213/functions/dotsdraw.m
+++ d213/functions/dotsdraw.m
@@ -19,32 +19,31 @@ sl = slist.list;            % 'sl' for save, 'so' for without save
 % seed [int] for random number generator
 % contrast [0, 1] contrast of each checker
 % framerate [Hz] frame per second
-stimnum = size(sl, 2);          % the total number of stimuli
+stimnum = size(sl, 2);     	% the total number of stimuli
 
 framebuffer = 0.5;
 
 try
     % check if the installed Psychtoolbox is based on OpenGL ('Screen()'),
-    %       and provide a consistent mapping of key codes
+    % and provide a consistent mapping of key codes
     AssertOpenGL;
     KbName('UnifyKeyNames');                        %  = PsychDefaultSetup(1);
     
-    %Screen('Preference', 'ScreenToHead', 1, 1, 0); % use this in a real experiment (outdated)
+    %Screen('Preference', 'ScreenToHead', 1, 1, 0); % (outdated) use this in a real experiment
     Screen('Preference', 'SkipSyncTests', 0);       % don't use ('SkipSyncTests', 1) in a real experiment
     
     % load KbCheck because it takes some time to read for the first time
     while KbCheck(); end
-    
-    ListenChar(2);                                  % suppress output of keypresses
+    istenChar(2);                                  	% suppress output of keypresses
     
     % get the screen numbers
     myScreen = sc.idx;
     
     % open an on screen window
     if myScreen == 1
-    	Screen('ConfigureDisplay', 'Scanout', myScreen, 0, 912, 1140);
+        Screen('ConfigureDisplay', 'Scanout', myScreen, 0, 912, 1140);	
         SetResolution(myScreen, 912, 1140, 60);
-    	[myWindow_, ~] = Screen('OpenWindow', myScreen - 1, uint8(255/2 * sc.ch));
+        [myWindow_, ~] = Screen('OpenWindow', myScreen - 1, uint8(255/2 * sc.ch));
         [myWindow, ~] = Screen('OpenWindow', myScreen, uint8(255/2 * sc.ch));
     else
         [myWindow, ~] = Screen('OpenWindow', myScreen, uint8(255/2 * sc.ch), sc.debugsize);
@@ -99,21 +98,21 @@ try
         
         % initialize boxes & photodiode
         [~, sl{s}.numColumns] = size(so{s}.boxes);
-        so{s}.boxColor = zeros(3, sl{s}.numColumns, sl{s}.totalFrame);      
-        so{s}.pdColor = zeros(3, sl{s}.totalFrame);                         
+        so{s}.boxColor = zeros(3, sl{s}.numColumns, sl{s}.totalFrame);
+        so{s}.pdColor = zeros(3, sl{s}.totalFrame);
         
         % prepare for drawing
         if sl{s}.name == "naturalmovie" || sl{s}.name == "naturalscene"
             m = loadmatfiles(sl{s});            % [X1 * X2, T]; [0, 255]
             m = m(:, 1:sl{s}.totalFrame);
-            so{s}.boxColor(1, :, :) = st.ch(1) * m(:, :);  
+            so{s}.boxColor(1, :, :) = st.ch(1) * m(:, :);
             so{s}.boxColor(2, :, :) = st.ch(2) * m(:, :);
             so{s}.boxColor(3, :, :) = st.ch(3) * m(:, :);
             so{s}.pdColor(1, :) = pd.ch(1) * m(1, :);
             so{s}.pdColor(2, :) = pd.ch(2) * m(1, :);
             so{s}.pdColor(3, :) = pd.ch(3) * m(1, :);
         else
-            rng(sl{s}.seed);                    % default = 0;
+            rng(sl{s}.seed);                    				% default = 0;
             % construct boxes and photodiodes
             for c = 1:sl{s}.totalFrame
                 boxSequence = rand(1, sl{s}.numColumns);		% [0, 1]
@@ -122,32 +121,32 @@ try
                 end
                 boxSequenceIntensity = boxSequence * 2 * (meanIntensity * sl{s}.contrast) ...
                     + meanIntensity * (1 - sl{s}.contrast);		% [0, 255]
-                so{s}.boxColor(1, :, c) = st.ch(1) * boxSequenceIntensity;  
+                so{s}.boxColor(1, :, c) = st.ch(1) * boxSequenceIntensity;
                 so{s}.boxColor(2, :, c) = st.ch(2) * boxSequenceIntensity;
                 so{s}.boxColor(3, :, c) = st.ch(3) * boxSequenceIntensity;
-                so{s}.pdColor(1, c) = pd.ch(1) * boxSequenceIntensity(1);  
+                so{s}.pdColor(1, c) = pd.ch(1) * boxSequenceIntensity(1);
                 so{s}.pdColor(2, c) = pd.ch(2) * boxSequenceIntensity(1);
                 so{s}.pdColor(3, c) = pd.ch(3) * boxSequenceIntensity(1);
             end
         end
         % upsample to make it 60 Hz
-        %so{s}.boxColor = uint8(upsample_s(so{s}.boxColor, 2, 3));                                 
+        %so{s}.boxColor = uint8(upsample_s(so{s}.boxColor, 2, 3));
         %so{s}.pdColor = uint8(upsample_s(0.05 * so{s}.pdColor, 2, 2));
-        so{s}.boxColor = uint8(repelem(so{s}.boxColor, 1, 1, 2));                                 
+        so{s}.boxColor = uint8(repelem(so{s}.boxColor, 1, 1, 2));
         so{s}.pdColor = uint8(repelem(0.05 * so{s}.pdColor, 1, 2));
         % first frame pd
         so{s}.pdColor(:, 1) = uint8(white * pd.ch);
-
+        
         % override the background color with photodiode (blending)
         for ch = 1:3
-        	if sc.ch(ch) == 1 && pd.ch(ch) == 0
-        		so{s}.pdColor(ch, :) = uint8(meanIntensity);
-        	end
+            if sc.ch(ch) == 1 && pd.ch(ch) == 0
+                so{s}.pdColor(ch, :) = uint8(meanIntensity);
+            end
         end
         
         % convert to dots and rotate the dots
         [so{s}.raw_dots, so{s}.dotColor] = boxColor2dotColor(so{s}.boxColor, sl{s}.boxSize);
-        so{s}.dots = rotate_dots(so{s}.raw_dots, st.offset(2), st.offset(1));     % offset(1;'x'=x2), offset(2;'y'=x1)
+        so{s}.dots = rotate_dots(so{s}.raw_dots, st.offset(2), st.offset(1));	% offset(1;'x'=x2), offset(2;'y'=x1)
     end
     
     % define black and white frame (blended with the background screen)
@@ -155,12 +154,12 @@ try
     whiteframe = defaultframe;
     blackframe = defaultframe;
     for ch = 1:3
-    	if pd.ch(ch) == 1
-		    whiteframe(ch) = white;
-		    blackframe(ch) = black;
-		end
-	end
-
+        if pd.ch(ch) == 1
+            whiteframe(ch) = white;
+            blackframe(ch) = black;
+        end
+    end
+    
     % prepare for the first screen
     Screen('FillOval', myWindow, blackframe, PHOTODIODE);
     Screen('Flip', myWindow);
@@ -176,7 +175,7 @@ try
     end
     Screen('FillOval', myWindow, blackframe, PHOTODIODE);
     Screen('Flip', myWindow);
-
+    
     % draw the list of stimuli
     for s = 1:stimnum
         boxes = so{s}.boxes;
@@ -194,13 +193,13 @@ try
         vbl = Screen('Flip', myWindow);
         Screen('FillOval', myWindow, blackframe, PHOTODIODE);
         vbl = Screen('Flip', myWindow);
-               
+        
         % i = 1:sl{s}.totalFrame60 (from 1st frame to last frame)
         for i = 1:sl{s}.totalFrame60
             % draw dots
-            %Screen('FillRect', myWindow, boxColor(:, :, i), boxes);
+            Screen('FillRect', myWindow, boxColor(:, :, i), boxes);	% this is for boxes
             Screen('DrawDots', myWindow, dots, 1, dotColor(:, :, i), [], 0);
-	    Screen('FillOval', myWindow, pdColor(:, i), PHOTODIODE);
+            Screen('FillOval', myWindow, pdColor(:, i), PHOTODIODE);
             [vbl, ~, ~, mbp] = Screen('Flip', myWindow, vbl + (1 - framebuffer) * ifi);
             if mbp > 0
                 %i
@@ -209,14 +208,14 @@ try
                 ms = ms + 1;
             end
         end
-
+        
         % i = sl{s}.totalFrame60 + 1 (after original frame, 'white pd')
         Screen('FillOval', myWindow, whiteframe, PHOTODIODE);
         [vbl, ~, ~, mbp] = Screen('Flip', myWindow, vbl + (1 - framebuffer) * ifi);
         
-		% print if there are flip misses
+        % print if there are flip misses
         flipmiss_temp(1:5, :)'
-
+        
         % after
         Screen('FillOval', myWindow, blackframe, PHOTODIODE);
         [vbl, ~, ~, mbp] = Screen('Flip', myWindow, vbl + (1 - framebuffer) * ifi);
@@ -228,7 +227,7 @@ try
         end
         
         % saves if there are (less than 100) flipmiss
-        sl{s}.flipmiss = reshape(nonzeros(flipmiss_temp), [], 2);       
+        sl{s}.flipmiss = reshape(nonzeros(flipmiss_temp), [], 2);
     end
     
     for p = 1:ceil(60*ti.pausetimeafter)
@@ -240,7 +239,7 @@ try
     Screen('CloseAll');
     ShowCursor();
     ListenChar(0);
-   
+    
     % save experiment configuration as .json file
     ev.mode = 'pattern/dotsdraw.m';
     ex{1} = ev;
