c74b82a691d91663b40ff7985f7138029faa7842
Dongsoo Lee <dongsoolee2@gmail.com>
2020-08-12 16:43:51 -0700

    d239 CRT monitor setup change

---
+++ d239/source.md
@@ -0,0 +1,7 @@
+CRT monitor
+MultiSync FE700 (16'; 1024 X 768; 60 Hz)
+
+Gamma Correction: 2.2 (20-08-12)
+
+With 10X top objective, 
+17 px = 100 um 
---
+++ d239/startup.m
@@ -0,0 +1,20 @@
+% add visual stimulus library to matlab path
+addpath(genpath('/home/dlee/Documents/MATLAB/visual-stimuli/d239'));
+addpath(genpath('/home/dlee/Documents/MATLAB/visual-stimuli/matrix'));
+addpath(genpath('/home/dlee/Documents/MATLAB/visual-stimuli/utils'));
+cd('/home/dlee/Documents/MATLAB/visual-stimuli/d239');
+
+% configure display resolution at start up (for LightCrafter4500 pattern mode)
+%Screen('ConfigureDisplay', 'Scanout', 1, 0, 912, 1140);
+%SetResolution(1, 912, 1140, 60);
+
+% configure display resolution at start up (for LightCrafter4500 video mode)
+%Screen('ConfigureDisplay', 'Scanout', 1, 0, 1280, 800);
+%SetResolution(1, 1280, 800, 60);
+
+% change output format
+format long;
+
+% clear
+clear all;
+close all;
