3a7751ce574557ce72b115952bfc0a0bca77fdad
Dongsoo Lee <dongsoolee2@gmail.com>
2020-08-17 20:57:10 -0700

    d239 CRT new setup was added to the startup.m

--- d239/startup.m
+++ d239/startup.m
@@ -4,6 +4,10 @@ addpath(genpath('/home/dlee/Documents/MATLAB/visual-stimuli/matrix'));
 addpath(genpath('/home/dlee/Documents/MATLAB/visual-stimuli/utils'));
 cd('/home/dlee/Documents/MATLAB/visual-stimuli/d239');
 
+% configure display resolution at start up (for CRT MultiSync FE700)
+Screen('ConfigureDisplay', 'Scanout', 0, 0, 1024, 768);
+SetResolution(0, 1024, 768, 60);
+
 % configure display resolution at start up (for LightCrafter4500 pattern mode)
 %Screen('ConfigureDisplay', 'Scanout', 1, 0, 912, 1140);
 %SetResolution(1, 912, 1140, 60);
